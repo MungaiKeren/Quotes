5b60b52ec6cbcc0f77266a7a1a2c80b625cedf9b
MungaiKeren <wambukeren@gmail.com>
2019-08-17 19:28:10 +0300

    This commit will add a form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 import { QuoteAgePipe } from './quote-age.pipe';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
     QuoteDetailsComponent,
-    QuoteAgePipe
+    QuoteAgePipe,
+    QuoteFormComponent
   ],
   imports: [
     BrowserModule,
