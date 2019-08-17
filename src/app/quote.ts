eaacf15c071f6f06807b2aab6886d7fcddb80f7e
MungaiKeren <wambukeren@gmail.com>
2019-08-17 14:22:52 +0300

    This commit will add the upvotes and downvotes counters

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,7 +1,11 @@
 export class Quote {
     seeDetails: boolean;
+    upvotes:number;
+    downvotes:number;
     constructor(public id:number, public name:string, public author:string,public personSubmitted:string,public dateCreated:Date){
         this.seeDetails = false;
+        this.upvotes=0;
+        this.downvotes=0;
     }
     
 }
