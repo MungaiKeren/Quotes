8111a55b0b6b54482ca88db5e82d1e13f92f09ff
MungaiKeren <wambukeren@gmail.com>
2019-08-18 00:04:40 +0300

    this commit  will add some custom stylings

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,9 +9,9 @@ import { Quote } from '../quote';
 export class QuoteComponent implements OnInit {
 
   quotes:Quote[] = [
-    new Quote(1, '. Every new level of your life will demand a different you','Mark Edward','Jane',new Date(2019,7,17)),
-    new Quote(2,'. Change the world by being yourself.','Jean Seon','Harriet',new Date(2019,0,17)),
-    new Quote(3,'. Be the best that you can ever be.','Jean Seon','Harriet',new Date(2019,11,16)),    
+    new Quote(1, 'Every new level of your life will demand a different you','Mark Edward','Jane',new Date(2019,7,17)),
+    new Quote(2,'Change the world by being yourself.','Jean Seon','Harriet',new Date(2019,0,17)),
+    new Quote(3,'Be the best that you can ever be.','Jean Seon','Harriet',new Date(2019,11,16)),    
   ];
 
 
