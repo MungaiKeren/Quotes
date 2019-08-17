d8d7f903b254addbf3142b7ca2c48cefffbd8853
MungaiKeren <wambukeren@gmail.com>
2019-08-17 14:13:39 +0300

    This commit will calculate the days since the quote was created

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -11,7 +11,7 @@ export class QuoteComponent implements OnInit {
   quotes:Quote[] = [
     new Quote(1, '. Every new level of your life will demand a different you','Mark Edward','Jane',new Date(2019,0,15)),
     new Quote(2,'. Change the world by being yourself.','Jean Seon','Harriet',new Date(2019,0,17)),
-    new Quote(3,'. Be the best that you can ever be.','Jean Seon','Harriet',new Date(2019,1,16)),    
+    new Quote(3,'. Be the best that you can ever be.','Jean Seon','Harriet',new Date(2019,11,16)),    
   ];
 
   seeDetails(index){
