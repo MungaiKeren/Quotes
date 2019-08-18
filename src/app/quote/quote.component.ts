946e64586ee1a3f36ca8c2917375b7c246e999e4
MungaiKeren <wambukeren@gmail.com>
2019-08-18 11:28:33 +0300

    This commit will add some new quotes

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -11,7 +11,10 @@ export class QuoteComponent implements OnInit {
   quotes:Quote[] = [
     new Quote(1, 'Every new level of your life will demand a different you','Mark Edward','Jane',new Date(2019,7,17)),
     new Quote(2,'Change the world by being yourself.','Jean Seon','Harriet',new Date(2019,0,17)),
-    new Quote(3,'Be the best that you can ever be.','Jean Seon','Harriet',new Date(2019,11,16)),    
+    new Quote(3,'Be the best that you can ever be.','Jean Seon','Harriet',new Date(2019,1,16)),    
+    new Quote(4,'We accept the love we think we deserve','Good reads','Kay',new Date(2019,2,13)),
+    new Quote(5,'Its going to be hard but hard doesnt mean its impossible','Successful thoughts','Jean',new Date(2019,6,12)),
+    new Quote(6,'Life is what happens when youre busy making other plans','Brainy Quote','Kay',new Date(2019,8,18)),
   ];
 
 
