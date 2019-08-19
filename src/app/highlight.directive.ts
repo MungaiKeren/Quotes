4a83e6f32a0f20f3e8a45dafc883b290fce4bc83
MungaiKeren <wambukeren@gmail.com>
2019-08-19 11:20:27 +0300

    This commit will add a highlight directive functionality

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,10 +1,24 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
 
-  constructor() { }
+  constructor(private elem:ElementRef) { }
+
+  @HostListener("click") onClicks(){
+    this.background("#0b032d");
+  }
+
+  @HostListener('dblclick')onDoubleClicks(){
+    this.background('none');
+  }
+
+  private background(action:string){
+    this.elem.nativeElement.style.background=action;
+  }
+    
+   
 
 }
